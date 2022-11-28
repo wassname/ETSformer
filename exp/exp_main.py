02b9e6f8d971aacbec3c02d7009fdff6cfdf96cd
wassname <git@wassname.org>
2022-11-29 06:26:01 +0800

    added notebook

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -237,4 +237,4 @@ class Exp_Main(Exp_Basic):
             np.save(folder_path + 'pred.npy', preds)
             np.save(folder_path + 'true.npy', trues)
 
-        return
+        return preds, trues
